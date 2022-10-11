79490a3b36e651607262122419a6e7d811953175
ClementCaton <clement.caton@algosup.com>
2022-10-11 12:04:12 +0200

    Better paths

--- clippy/webserver/main.go
+++ clippy/webserver/main.go
@@ -21,23 +21,6 @@ type Player struct {
 	Team       string
 }
 
-var players = map[string]int{
-	"Pepper":      20,
-	"Jhon":        20,
-	"Brendon":     10,
-	"Louis":       30,
-	"Harry":       40,
-	"Zayn":        50,
-	"Niall":       60,
-	"Mia":         70, //Théo's sister
-	"Taylor":      30,
-	"Mary":        10,
-	"Maris":       20,
-	"Maryoumaris": 30,
-	"Jhonny":      30,
-	"Sins":        40,
-}
-
 func main() {
 	http.HandleFunc("/players/", PlayerServer)
 	http.ListenAndServe(":8080", nil)
@@ -54,6 +37,33 @@ func parsePlayers() Players {
 	return players
 }
 
+func getPlayerList() string {
+	//	var playerList string
+	//	players := parsePlayers()
+	//	for _, player := range players.Players {
+	//		playerList += "{" + player.Name + ", " + strconv.Itoa(player.Wins) + ", " + strconv.Itoa(player.TotalGames) + ", " + player.Team + "},\n"
+	//	}
+	//	return playerList
+
+	message := ""
+	var players Players
+	jsonFile, err := os.Open("./players.json")
+	if err != nil {
+		panic(err)
+	}
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	json.Unmarshal(byteValue, &players)
+	message = "["
+	for i, player := range players.Players {
+		message += player.Name
+		if i != len(players.Players)-1 {
+			message += ", "
+		}
+	}
+	message += "]"
+	return message
+}
+
 func getPlayerScore(name string) int {
 	players := parsePlayers()
 	for _, player := range players.Players {
@@ -62,6 +72,26 @@ func getPlayerScore(name string) int {
 		}
 	}
 	return -1
+}
+
+func getPlayerTotalGames(name string) int {
+	players := parsePlayers()
+	for _, player := range players.Players {
+		if player.Name == name {
+			return player.TotalGames
+		}
+	}
+	return -1
+}
+
+func getPlayerTeam(name string) string {
+	players := parsePlayers()
+	for _, player := range players.Players {
+		if player.Name == name {
+			return player.Team
+		}
+	}
+	return "No team"
 }
 
 func URLSplit(url string) []string {
@@ -96,18 +126,27 @@ func getURLCategory(url string) string {
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
 	//base := getURLbase(r.URL.Path)
 	player := getURLPlayer(r.URL.Path)
-	//category := getURLCategory(r.URL.Path)
+	category := strings.ToLower(getURLCategory(r.URL.Path))
 
-	score := getPlayerScore(player)
+	message := ""
+
+	switch category {
+	case "wins":
+		message = strconv.Itoa(getPlayerScore(player))
+	case "totalgames":
+		message = strconv.Itoa(getPlayerTotalGames(player))
+	case "team":
+		message = getPlayerTeam(player)
+	case "/":
+		message = getPlayerList()
+	default:
+		message = "{\"Name:\":" + player + ", \"Wins:\":" + strconv.Itoa(getPlayerScore(player)) + ", \"TotalGames:\":" + strconv.Itoa(getPlayerTotalGames(player)) + ", \"Team:\":\"" + getPlayerTeam(player) + "\"}"
+	}
 
 	switch r.Method {
 	case http.MethodGet:
 		w.WriteHeader(http.StatusOK)
-		if score != -1 {
-			w.Write([]byte(strconv.Itoa(score)))
-		} else {
-			w.Write([]byte("This player does not exist"))
-		}
+		w.Write([]byte(message))
 	case http.MethodPost:
 		w.WriteHeader(http.StatusCreated)
 		//increase the player's score
