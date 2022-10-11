83f7be3ccd21ca2f369a04004547bc35bacbab22
Guillaume-Riviere <91249622+Guillaume-Riviere@users.noreply.github.com>
2022-10-11 12:21:22 +0200

    Refactoring

--- Teeam25/main.go
+++ Teeam25/main.go
@@ -2,17 +2,21 @@ package main
 
 import "math"
 
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
-func (r Rectangle) Area() float64 {
-	return r.Width * r.Height
-}
-
 type Circle struct {
 	Radius float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
 }
 
 func (c Circle) Area() float64 {
