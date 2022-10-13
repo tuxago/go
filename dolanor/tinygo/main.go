7e1ea6b00b547743d62ecc4308fce76e66521381
Tanguy ⧓ Herrmann <dolanorgit@evereska.org>
2022-10-13 11:50:11 +0200

    refactor: extract machine code for led into hw/led package

--- dolanor/tinygo/main.go
+++ dolanor/tinygo/main.go
@@ -2,46 +2,19 @@ package main
 
 import (
 	"fmt"
-	"machine"
 	"time"
+
+	"github.com/tuxago/go/dolanor/tinygo/hw/led"
 )
 
 func main() {
 	blink()
 }
 
-type LED struct {
-	led machine.Pin
-	on  bool
-}
-
-func (l *LED) On() {
-	l.led.High()
-	l.on = true
-}
-
-func (l *LED) Off() {
-	l.led.Low()
-	l.on = false
-}
-
-func (l *LED) Toggle() {
-	if !l.on {
-		l.On()
-	} else {
-		l.Off()
-	}
-	//toggleLED(&l.led, &l.on)
-}
-
 func blink() {
-	led := machine.LED
-	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
 	ledCtrl := make(chan bool, 10)
 
-	l := LED{
-		led: led,
-	}
+	l := led.New()
 
 	go func() {
 		for {
